69162c71a22957662e9675ddb8a2d1f07a8bc095
Henning-Winker <henning.winker@gmail.com>
2020-06-09 00:55:33 +0200

    adjusted omi

--- R/Utils.R
+++ R/Utils.R
@@ -9,7 +9,7 @@
 sspar <- function(mfrow=c(1,1),plot.cex=1,mai=c(0.5,0.5,0.1,.1),omi = c(0.,0.,0.,0)+ 0.05,labs=FALSE){
   if(labs==F){
     mai=c(0.25,0.25,0.15,.15)
-    omi = c(0.3,0.35,0.2,0)}
+    omi = c(0.3,0.35,0.2,0.2)}
   par(list(mfrow=mfrow,mai = mai, mgp =c(2.,0.5,0),omi =omi, tck = -0.02,cex=plot.cex))
 }
 

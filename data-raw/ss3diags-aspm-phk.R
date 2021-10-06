77fcff42190691c3d812c4f9612fa59eaa3f2c0b
efletcherPIFSC <37851243+efletcherPIFSC@users.noreply.github.com>
2021-10-06 09:31:46 -1000

    Reproducablity Scripts

---
+++ data-raw/ss3diags-aspm-phk.R
@@ -0,0 +1,3 @@
+## code to prepare `ss3diags::aspm.phk` dataset goes here
+
+usethis::use_data(ss3diags::aspm.phk, overwrite = TRUE)
---
+++ data-raw/ss3diags-retro-sma.R
@@ -0,0 +1,3 @@
+## code to prepare `ss3diags::retro.sma` dataset goes here
+
+usethis::use_data(ss3diags::retro.sma, overwrite = TRUE)

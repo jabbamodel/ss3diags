922e6c75f8de87c70e1f8ca979c1b6a233701269
MOshima-PIFSC <megumi.oshima@noaa.gov>
2021-11-03 13:04:12 -0400

    minor changes

--- tests/testthat/test-uncertainty.R
+++ tests/testthat/test-uncertainty.R
@@ -1,6 +1,6 @@
 library(ss3diags)
 library(testthat)
-library(dplyr)
+
 
 ss3diags::ss3sma
 
@@ -17,23 +17,23 @@ test_that("mle table values are as expected for SMA with Fref = MSY", {
 
 test_that("Kobe plot values are correct", {
   
-  kb <- mvln.msy$kb %>% 
-    filter(year == 2015) %>% 
-    mutate(green = ifelse(stock > 1 & harvest < 1, 1, 0), 
-           red = ifelse(stock < 1 & harvest > 1, 1, 0), 
-           yellow = ifelse(stock < 1 & harvest < 1, 1, 0),
-           orange = ifelse(stock > 1 & harvest > 1, 1, 0)) %>% 
-    summarise(pgreen = sum(green)/nrow(.)*100,
-              pred = sum(red)/nrow(.)*100,
-              pyellow = sum(yellow)/nrow(.)*100,
-              porange = sum(orange)/nrow(.)*100)
+  kb <- mvln.msy$kb[which(mvln.msy$kb$year == 2015),]
+    
+  kb$green = ifelse(kb$stock > 1 & kb$harvest < 1, 1, 0) 
+  kb$red = ifelse(kb$stock < 1 & kb$harvest > 1, 1, 0) 
+  kb$yellow = ifelse(kb$stock < 1 & kb$harvest < 1, 1, 0)
+  kb$orange = ifelse(kb$stock > 1 & kb$harvest > 1, 1, 0) 
+  pgreen = sum(kb$green)/nrow(kb)*100
+  pred = sum(kb$red)/nrow(kb)*100
+  pyellow = sum(kb$yellow)/nrow(kb)*100
+  porange = sum(kb$orange)/nrow(kb)*100
   
   kb.p <- SSplotKobe(mvln.msy$kb)
   
-  expect_equal(kb.p$Percent[1], kb$pred)
-  expect_equal(kb.p$Percent[2], kb$porange)
-  expect_equal(kb.p$Percent[3], kb$pyellow)
-  expect_equal(kb.p$Percent[4], kb$pgreen)
+  expect_equal(kb.p$Percent[1], pred)
+  expect_equal(kb.p$Percent[2], porange)
+  expect_equal(kb.p$Percent[3], pyellow)
+  expect_equal(kb.p$Percent[4], pgreen)
   
 })
 

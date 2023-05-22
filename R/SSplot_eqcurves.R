47469142cf46944f360eeb2523ddc07a2100f302
jabbamodel <jabba.themodel@gmail.com>
2023-05-22 15:49:48 +0200

    refpts for eqcurves

--- R/SSplot_eqcurves.R
+++ R/SSplot_eqcurves.R
@@ -16,7 +16,7 @@
 #' SSplot_eqcurves(ss3phk,Fref="Btgt") 
 #' # SPR40 refpoints 
 #' SSplot_eqcurves(ss3phk,Fref="SPR") 
-SSplot_eqcurves = function(ss3rep,Fref=c("MSY","Btgt","SPR","F01")[2],msyline=NULL,cex=0.7){ 
+SSplot_eqcurves = function(ss3rep,Fref=c("MSY","Btgt","SPR","F01")[2],plot=TRUE,verbose=TRUE, msyline=NULL,cex=0.7){ 
   
   eq = ss3rep$equil_yield
   b = eq$SSB
@@ -65,31 +65,42 @@ SSplot_eqcurves = function(ss3rep,Fref=c("MSY","Btgt","SPR","F01")[2],msyline=NU
     rich.vector <- apply(rgb.m, 1, function(v) rgb(v[1], v[2], v[3], alpha=alpha))
   }
   
+  
+  if(plot){
   cols= rc(6)
   ylim = c(0,max(y)*1.1)
   mai=c(0.55,0.25,0.15,.15)
   omi = c(0.3,0.35,0.2,0.2)
   par(mfrow=c(1,2),mai = mai, mgp =c(2.,0.5,0),omi =omi, tck = -0.02,cex=cex)
-  plot(b,y,ylab="Yield",xlab="SSB",type="l",col=rc(1),lwd=2,ylim=ylim)
-  lines(c(bmsy,bmsy),c(0,ymsy),col=rc(1))
-  text(bmsy*0.85,ymsy*1.05,expression(B[MSY]),cex=cex+0.1)
-  if(Fref!="MSY"){
-  lines(c(btgt,btgt),c(0,ytgt),col=cols[3])
-  text(btgt*1.05,ytgt*1.05,bquote("B"[.(trg)]),cex=cex+0.1)
-  }
-  if(!is.null(msyline)) abline(h=msyline*ymsy,lty=2,col=2)
   
   
   xlim= c(0,max(f[y>0]))
   plot(f,y,ylab="Yield",xlab="Fishing Mortality",type="l",col=rc(1),lwd=2,ylim=ylim,xlim=xlim)
   lines(c(fmsy,fmsy),c(0,ymsy),col=rc(1))
-  text(fmsy*1.05,ymsy*1.05,expression(F[MSY]),cex=cex+0.1)
+  text(fmsy*1.15,ymsy*1.05,expression(F[MSY]),cex=cex+0.1)
   if(Fref!="MSY"){
     lines(c(ftgt,ftgt),c(0,ytgt),col=cols[3])
-    text(ftgt*0.95,ytgt*1.05,bquote("F"[.(trg)]),cex=cex+0.1)
+    text(ftgt*0.8,ytgt*1.05,bquote("F"[.(trg)]),cex=cex+0.1)
   }
   if(!is.null(msyline)) abline(h=msyline*ymsy,lty=2,col=2)
   mtext("Yield",outer = T,side=2,cex=cex)
-}
+
+  plot(b,y,ylab="Yield",xlab="SSB",type="l",col=rc(1),lwd=2,ylim=ylim)
+  lines(c(bmsy,bmsy),c(0,ymsy),col=rc(1))
+  text(bmsy*0.8,ymsy*1.05,expression(B[MSY]),cex=cex+0.1)
+  if(Fref!="MSY"){
+    lines(c(btgt,btgt),c(0,ytgt),col=cols[3])
+    text(btgt*1.15,ytgt*1.05,bquote("B"[.(trg)]),cex=cex+0.1)
+  }
+  if(!is.null(msyline)) abline(h=msyline*ymsy,lty=2,col=2)
+  }
+  
+  
+  refpts = data.frame(Fmsy=fmsy,Bmsy=bmsy,MSY=ymsy,B0=b0,R0=r0)
+  if(Fref!="MSY"){
+    refpts = cbind(refpts,data.frame(Ftgt=ftgt,Btgt=btgt,Ytgt = ytgt))
+  }
+  if(verbose) return(refpts)  
+  }
 
 

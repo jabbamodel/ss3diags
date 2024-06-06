8b8c6c55eb6f2621ff5bd00d4c22b0c102efdd31
jabbamodel <jabba.themodel@gmail.com>
2024-06-06 22:55:21 +0200

    add ss3.par in SSnewrun

--- R/SSnew_run.R
+++ R/SSnew_run.R
@@ -9,21 +9,22 @@
 #' @param ss.exe name of ss.exe file, default "control.exe"
 #' @param newdir file path to copy the ss3 files to
 #' @param overwrite if TRUE existingg files are overwritten
-#' @param pars if TRUE the "ss.par" is copied
+#' @param pars if TRUE the "ss3.par" is copied
+#' @param par.file par file name, default ss3.par
 #' @author Henning Winker
 #' @export
 
-SSnewrun <- function(model,dat,ctl,ss.exe="SS.exe",newdir,overwrite=TRUE,pars=TRUE){
+SSnewrun <- function(model,dat,ctl,ss.exe="ss3.exe",newdir,overwrite=TRUE,pars=FALSE,par.file="ss3.par"){
   dir.create(path = newdir , showWarnings = F)
   model.run = model
   # Copy files
   file.copy(file.path(model.run, "starter.ss_new"), file.path(newdir, "starter.ss"),overwrite=overwrite)
-  file.copy(file.path(model.run, "control.ss_new"), file.path(newdir, ctl),overwrite=overwrite)
+  file.copy(file.path(model.run, ctl), file.path(newdir, ctl),overwrite=overwrite)
   file.copy(file.path(model.run, dat), file.path(newdir, dat),overwrite=overwrite)
   file.copy(file.path(model.run, "forecast.ss"), file.path(newdir, "forecast.ss"),overwrite=overwrite)
   file.copy(file.path(model.run, ss.exe), file.path(newdir, ss.exe),overwrite=overwrite)
   # Automatically ignored for models without wtatage.ss
   file.copy(file.path(model.run, "wtatage.ss"), file.path(newdir, "wtatage.ss"),overwrite=overwrite)
   # add parameters
-  if(pars) file.copy(file.path(model.run, "ss.par"), file.path(newdir, "ss.par"),overwrite=overwrite)
+  if(pars) file.copy(file.path(model.run, par.file), file.path(newdir, "ss.par"),overwrite=overwrite)
 } # end function
